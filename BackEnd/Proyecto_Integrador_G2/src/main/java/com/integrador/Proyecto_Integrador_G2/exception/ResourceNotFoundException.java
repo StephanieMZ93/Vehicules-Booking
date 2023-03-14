d181da52d4a40f0c8147213c3873f16bab65bd70
Stephanie Martinez Zuluaga <105985136+StephanieMZ93@users.noreply.github.com>
2023-03-14 11:37:21 -0500

    Delete BackEnd/Proyecto_Integrador_G2 directory

--- BackEnd/Proyecto_Integrador_G2/src/main/java/com/integrador/Proyecto_Integrador_G2/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.integrador.Proyecto_Integrador_G2.exception;
-
-public class ResourceNotFoundException extends Exception {
-    public ResourceNotFoundException(String message){
-        super(message);
-    }
-}
